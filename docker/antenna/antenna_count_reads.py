57b4f5bf15138dfdb9fadce34c255d3e6cc5f03e
Nikolas Barkas <nikolas.barkas@outlook.com>
2021-11-08 17:41:44 +0000

    add detailed read counting

--- docker/antenna/antenna_count_reads.py
+++ docker/antenna/antenna_count_reads.py
@@ -5,6 +5,17 @@ import functools
 import collections
 import pandas as pd
 
+# Masks for output annotation
+TRS_3_PRIME_RC = 0x1 << 7
+TRS_3_PRIME_C = 0x1 << 6
+TRS_3_PRIME_R = 0x1 << 5
+TRS_3_PRIME_O = 0x1 << 4
+
+TRS_5_PRIME_RC = 0x1 << 3
+TRS_5_PRIME_C = 0x1 << 2
+TRS_5_PRIME_R = 0x1 << 1
+TRS_5_PRIME_O = 0x1 << 0
+
 
 @functools.total_ordering
 class bed_interval:
@@ -76,7 +87,143 @@ class bed_intervals:
         return None
 
 
-def simple_count_sgRNA(bamfilename, bedfile, sgRNA_bam_tag_name="TS"):
+def count_sgRNA(bamfilename, bedfile, sgRNA_bam_tag_name="TS"):
+    """Count sgRNAs stratifying by orientation"""
+    trs_intervals = bed_intervals()
+    trs_intervals.load_bed(bedfile)
+
+    def count_read(read, trs_intervals, sgRNA_bam_tag_name):
+        read_interval = trs_intervals.get_overlapping_interval(read.reference_start)
+        if read_interval is not None:
+            if read.has_tag(sgRNA_bam_tag_name):
+                trs_tag_info = read.get_tag(sgRNA_bam_tag_name)
+                read_interval["sg_count"] += 1
+                if read.is_read1:
+                    if trs_tag_info & TRS_5_PRIME_O:
+                        read_interval["sg_count_read1_5p_o"] += 1
+                    if trs_tag_info & TRS_5_PRIME_R:
+                        read_interval["sg_count_read1_5p_r"] += 1
+                    if trs_tag_info & TRS_5_PRIME_C:
+                        read_interval["sg_count_read1_5p_c"] += 1
+                    if trs_tag_info & TRS_5_PRIME_RC:
+                        read_interval["sg_count_read1_5p_rc"] += 1
+                    if trs_tag_info & TRS_3_PRIME_O:
+                        read_interval["sg_count_read1_3p_o"] += 1
+                    if trs_tag_info & TRS_3_PRIME_R:
+                        read_interval["sg_count_read1_3p_r"] += 1
+                    if trs_tag_info & TRS_3_PRIME_C:
+                        read_interval["sg_count_read1_3p_c"] += 1
+                    if trs_tag_info & TRS_3_PRIME_RC:
+                        read_interval["sg_count_read1_3p_rc"] += 1
+                if read.is_read2:
+                    if trs_tag_info & TRS_5_PRIME_O:
+                        read_interval["sg_count_read2_5p_o"] += 1
+                    if trs_tag_info & TRS_5_PRIME_R:
+                        read_interval["sg_count_read2_5p_r"] += 1
+                    if trs_tag_info & TRS_5_PRIME_C:
+                        read_interval["sg_count_read2_5p_c"] += 1
+                    if trs_tag_info & TRS_5_PRIME_RC:
+                        read_interval["sg_count_read2_5p_rc"] += 1
+                    if trs_tag_info & TRS_3_PRIME_O:
+                        read_interval["sg_count_read2_3p_o"] += 1
+                    if trs_tag_info & TRS_3_PRIME_R:
+                        read_interval["sg_count_read2_3p_r"] += 1
+                    if trs_tag_info & TRS_3_PRIME_C:
+                        read_interval["sg_count_read2_3p_c"] += 1
+                    if trs_tag_info & TRS_3_PRIME_RC:
+                        read_interval["sg_count_read2_3p_rc"] += 1
+            else:
+                read_interval["non_sg_count"] += 1
+
+    with pysam.AlignmentFile(bamfilename, "rb", require_index=True) as bamfile:
+        total_input_reads = bamfile.mapped + bamfile.unmapped
+        for read in tqdm.tqdm(bamfile, total=total_input_reads, smoothing=0):
+            if read.seq is None or read.is_unmapped:
+                continue
+            count_read(read, trs_intervals, sgRNA_bam_tag_name)
+
+    return trs_intervals
+
+
+def summarize_trs_intervals(trs_intervals):
+    # Summarize to pandas df
+    names = []
+    refs = []
+    starts = []
+    ends = []
+    sg_counts = []
+    sg_count_read1_5p_o_l = []
+    sg_count_read1_5p_r_l = []
+    sg_count_read1_5p_c_l = []
+    sg_count_read1_5p_rc_l = []
+    sg_count_read1_3p_o_l = []
+    sg_count_read1_3p_r_l = []
+    sg_count_read1_3p_c_l = []
+    sg_count_read1_3p_rc_l = []
+    sg_count_read2_5p_o_l = []
+    sg_count_read2_5p_r_l = []
+    sg_count_read2_5p_c_l = []
+    sg_count_read2_5p_rc_l = []
+    sg_count_read2_3p_o_l = []
+    sg_count_read2_3p_r_l = []
+    sg_count_read2_3p_c_l = []
+    sg_count_read2_3p_rc_l = []
+    non_sg_counts = []
+    for interval in trs_intervals.intervals:
+        names.append(interval.name)
+        refs.append(interval.ref)
+        starts.append(interval.start)
+        ends.append(interval.end)
+        sg_counts.append(interval["sg_count"])
+        sg_count_read1_5p_o_l.append(interval["sg_count_read1_5p_o"])
+        sg_count_read1_5p_r_l.append(interval["sg_count_read1_5p_r"])
+        sg_count_read1_5p_c_l.append(interval["sg_count_read1_5p_c"])
+        sg_count_read1_5p_rc_l.append(interval["sg_count_read1_5p_rc"])
+        sg_count_read1_3p_o_l.append(interval["sg_count_read1_3p_o"])
+        sg_count_read1_3p_r_l.append(interval["sg_count_read1_3p_r"])
+        sg_count_read1_3p_c_l.append(interval["sg_count_read1_3p_c"])
+        sg_count_read1_3p_rc_l.append(interval["sg_count_read1_3p_rc"])
+        sg_count_read2_5p_o_l.append(interval["sg_count_read2_5p_o"])
+        sg_count_read2_5p_r_l.append(interval["sg_count_read2_5p_r"])
+        sg_count_read2_5p_c_l.append(interval["sg_count_read2_5p_c"])
+        sg_count_read2_5p_rc_l.append(interval["sg_count_read2_5p_rc"])
+        sg_count_read2_3p_o_l.append(interval["sg_count_read2_3p_o"])
+        sg_count_read2_3p_r_l.append(interval["sg_count_read2_3p_r"])
+        sg_count_read2_3p_c_l.append(interval["sg_count_read2_3p_c"])
+        sg_count_read2_3p_rc_l.append(interval["sg_count_read2_3p_rc"])
+        non_sg_counts.append(interval["non_sg_count"])
+
+    return pd.DataFrame(
+        {
+            "name": names,
+            "ref": refs,
+            "start": starts,
+            "end": ends,
+            "sg_count": sg_counts,
+            "sg_count_read1_5p_o": sg_count_read1_5p_o_l,
+            "sg_count_read1_5p_r": sg_count_read1_5p_r_l,
+            "sg_count_read1_5p_c": sg_count_read1_5p_c_l,
+            "sg_count_read1_5p_rc": sg_count_read1_5p_rc_l,
+            "sg_count_read1_3p_o": sg_count_read1_3p_o_l,
+            "sg_count_read1_3p_r": sg_count_read1_3p_r_l,
+            "sg_count_read1_3p_c": sg_count_read1_3p_c_l,
+            "sg_count_read1_3p_rc": sg_count_read1_3p_rc_l,
+            "sg_count_read2_5p_o": sg_count_read2_5p_o_l,
+            "sg_count_read2_5p_r": sg_count_read2_5p_r_l,
+            "sg_count_read2_5p_c": sg_count_read2_5p_c_l,
+            "sg_count_read2_5p_rc": sg_count_read2_5p_rc_l,
+            "sg_count_read2_3p_o": sg_count_read2_3p_o_l,
+            "sg_count_read2_3p_r": sg_count_read2_3p_r_l,
+            "sg_count_read2_3p_c": sg_count_read2_3p_c_l,
+            "sg_count_read2_3p_rc": sg_count_read2_3p_rc_l,
+            "non_sg_counts": non_sg_counts,
+        }
+    )
+
+
+def simple_count_sgRNA(
+    bamfilename, bedfile, sgRNA_bam_tag_name="TS", count_read_1=True, count_read_2=False
+):
     """
     Count sgRNA reads in the most simple way possible
     
@@ -89,26 +236,24 @@ def simple_count_sgRNA(bamfilename, bedfile, sgRNA_bam_tag_name="TS"):
     trs_intervals = bed_intervals()
     trs_intervals.load_bed(bedfile)
 
+    def count_read(read, trs_intervals, sgRNA_bam_tag_name):
+        read_interval = trs_intervals.get_overlapping_interval(read.reference_start)
+        if read_interval is not None:
+            if read.has_tag(sgRNA_bam_tag_name):
+                read_interval["sg_count"] += 1
+            else:
+                read_interval["non_sg_count"] += 1
+
     with pysam.AlignmentFile(bamfilename, "rb", require_index=True) as bamfile:
         total_input_reads = bamfile.mapped + bamfile.unmapped
         for read in tqdm.tqdm(bamfile, total=total_input_reads, smoothing=0):
             # Ignore reads without a sequence or that are unmapped
             if read.seq is None or read.is_unmapped:
                 continue
-            if read.is_read1:
-                read_interval = trs_intervals.get_overlapping_interval(
-                    read.reference_start
-                )
-                if read_interval is not None:
-                    if read.has_tag(sgRNA_bam_tag_name):
-                        read_interval["sg_count"] += 1
-                    else:
-                        read_interval["non_sg_count"] += 1
-                else:
-                    continue
-            else:
-                # Ignore non-read 1
-                continue
+            if read.is_read1 and count_read_1:
+                count_read(read, trs_intervals, sgRNA_bam_tag_name)
+            if read.is_read2 and count_read_2:
+                count_read(read, trs_intervals, sgRNA_bam_tag_name)
 
     # Summarize to pandas df
     names = []
